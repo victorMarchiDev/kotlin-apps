4eedb11f91db26c9c3120126042dab82c30cbb1d
Victor Marchi <101611816+victorMarchiDev@users.noreply.github.com>
2025-04-13 20:32:12 -0300

    Criando um Aplicativo de sorteio simples com Kotlin

--- PrimeiroApp/app/src/main/java/com/example/primeiroapp/MainActivity.kt
+++ PrimeiroApp/app/src/main/java/com/example/primeiroapp/MainActivity.kt
@@ -16,10 +16,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
+    //Função para sortear um número e atribui-lo ao textView
+
     fun sortear(view: View){
         val textoResultado = findViewById<TextView>(R.id.text_resultado)
         val numero = java.util.Random().nextInt(11)
         textoResultado.setText("número gerado: $numero")
     }
+
 
 }
