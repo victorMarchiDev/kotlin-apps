0573ed27c63367836a404e3f772f3c028b2b5667
Victor Marchi <101611816+victorMarchiDev@users.noreply.github.com>
2025-04-13 20:31:05 -0300

    Initial commit

---
+++ PrimeiroApp/app/src/main/java/com/example/primeiroapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.primeiroapp
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sortear(view: View){
+        val textoResultado = findViewById<TextView>(R.id.text_resultado)
+        val numero = java.util.Random().nextInt(11)
+        textoResultado.setText("número gerado: $numero")
+    }
+
+}
