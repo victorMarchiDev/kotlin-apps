069ae5a974d6f5a9701eb0d52ba0d4bcd2dfe008
Victor Marchi <101611816+victorMarchiDev@users.noreply.github.com>
2025-04-14 16:13:14 -0300

    Adicionando logica para gerar mensagens aleatorias

--- Frases-Do-Dia/app/src/main/java/com/victormarchidev/frases_do_dia/MainActivity.java
+++ Frases-Do-Dia/app/src/main/java/com/victormarchidev/frases_do_dia/MainActivity.java
@@ -1,6 +1,9 @@
 package com.victormarchidev.frases_do_dia;
 
 import android.os.Bundle;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    String[] frases = {
+      "Acredite em si mesmo e você será imparável",
+      "A persistência é o caminho do êxito",
+      "O sucesso é a soma de pequenos esforços repetidos dia após dia"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,4 +27,24 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
     }
+
+    public void gerarFrase(View view){
+        TextView texto = findViewById(R.id.textResultado);
+        int numeroAleatorio = new Random().nextInt(3);
+        String frase = frases[numeroAleatorio];
+        texto.setText(frase);
+    }
+
+    public void exibirTodas(View view){
+
+        TextView resultado = findViewById(R.id.textResultado);
+        String textoResultado = "";
+
+        for(String frase : frases){
+            textoResultado = textoResultado + frase + "\n";
+        }
+
+        resultado.setText(textoResultado);
+    }
+
 }
