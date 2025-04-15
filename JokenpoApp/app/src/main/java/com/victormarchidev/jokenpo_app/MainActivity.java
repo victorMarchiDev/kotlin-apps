0aa421b53b1eee9974e4938107634c8ba7c92dee
Victor Marchi <101611816+victorMarchiDev@users.noreply.github.com>
2025-04-15 15:55:54 -0300

    Criando logica para geracao de escolha aleatoria e verificacao do ganhador

--- JokenpoApp/app/src/main/java/com/victormarchidev/jokenpo_app/MainActivity.java
+++ JokenpoApp/app/src/main/java/com/victormarchidev/jokenpo_app/MainActivity.java
@@ -2,12 +2,15 @@ package com.victormarchidev.jokenpo_app;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,7 +33,33 @@ public class MainActivity extends AppCompatActivity {
         verificarGanhador("Tesoura");
     }
 
-    private void verificarGanhador(String escolhaUsuario){
-        System.out.println("item clicado: " + escolhaUsuario);
+    private String gerarEscolhaAleatoria() {
+        String[] opcoes = {"Pedra", "Papel", "Tesoura"};
+        int numeroAleatorio = new Random().nextInt(3);
+
+        ImageView imagemApp = findViewById(R.id.image_app);
+
+        String escolhaApp = opcoes[numeroAleatorio];
+        switch (escolhaApp){
+            case "Pedra":
+                imagemApp.setImageResource(R.drawable.pedra);
+                break;
+            case "Papel":
+                imagemApp.setImageResource(R.drawable.papel);
+                break;
+            case "Tesoura":
+                imagemApp.setImageResource(R.drawable.tesoura);
+                break;
+        }
+        return escolhaApp;
     }
+
+    private void verificarGanhador(String escolhaUsuario){
+
+        String escolhaApp = gerarEscolhaAleatoria();
+
+        //System.out.println("item clicado: " + escolhaUsuario);
+    }
+
+
 }
